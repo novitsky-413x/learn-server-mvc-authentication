7e649bde55670b09c47b8f17e8bc781f5fd4a5bc
Alexander Novitsky <novitsky.413x@gmail.com>
2025-04-01 15:35:32 +0300

    Initial commit

---
+++ controllers/auth.js
@@ -0,0 +1,46 @@
+const User = require('../models/user');
+
+exports.getLogin = (req, res, next) => {
+    // const isLoggedIn =
+    //     req
+    //         .get('Cookie')
+    //         .split(';')
+    //         .find((c) => c.trim().startsWith('loggedIn'))
+    //         .split('=')[1] === 'true';
+    res.render('auth/login', {
+        path: '/login',
+        pageTitle: 'Login',
+        isAuthenticated: false,
+    });
+};
+
+exports.getSignup = (req, res, next) => {
+    res.render('auth/signup', {
+        path: '/signup',
+        pageTitle: 'Signup',
+        isAuthenticated: false,
+    });
+};
+
+exports.postLogin = (req, res, next) => {
+    // res.setHeader('Set-Cookie', 'loggedIn=true; HttpOnly');
+    User.findById('67d05ac9e95a8e3a84bf6873')
+        .then((user) => {
+            req.session.isLoggedIn = true;
+            req.session.user = user;
+            req.session.save((err) => {
+                if (err) console.log(err);
+                res.redirect('/');
+            });
+        })
+        .catch((err) => console.log(err));
+};
+
+exports.postSignup = (req, res, next) => {};
+
+exports.postLogout = (req, res, next) => {
+    req.session.destroy((err) => {
+        if (err) console.log(err);
+        res.redirect('/');
+    });
+};
